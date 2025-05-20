0c5335a160e8f90478969c99f5ab94135f3f027d
rohan <rohanrv2004@gmail.com>
2025-05-20 19:04:24 +0530

    ui

--- layouts/AuthLayout.tsx
+++ layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-zinc-900 to-zinc-900">
       <div className="w-full max-w-md bg-zinc-900 shadow-md p-6 rounded-xl">
-        <Link href="/" className="block mb-6 text-center font-serif  transparent  text-3xl">
+        <Link href="/" className="block mb-6 text-center font-serif text-gray-200 transparent  text-3xl">
           SkillShare
         </Link>
         {children}
